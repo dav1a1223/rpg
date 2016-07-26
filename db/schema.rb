9588186263b28d602fa4e464e5e73a98521fda46
dalai <dav1a1223@gmail.com>
2016-07-27 03:54:27 +0800

    remove sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160725130533) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 20160725130533) do
     t.string   "pwd_image"
   end
 
-  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,7 +49,8 @@ ActiveRecord::Schema.define(version: 20160725130533) do
     t.string   "number"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "tasks", "users"
 end
